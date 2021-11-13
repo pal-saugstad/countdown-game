29da0fc89c4bc597924c77027679c7532eb10764
Pål Saugstad <pal@saugstad.net>
2021-11-13 09:14:03 +0100

    Beautify, no change

--- cntdn.js
+++ cntdn.js
@@ -27,12 +27,29 @@ var abs_diff;
 var calculations = 0;
 
 var OPS = [
-    function(n1, n2) { return [n1[0]+n2[0], '+', n2, n1]; },
-    function(n1, n2) { if (n2[0] >= n1[0]) return false; return [n1[0]-n2[0], '-', n1, n2]; },
-    function(n2, n1) { if (n2[0] >= n1[0]) return false; return [n1[0]-n2[0], '-', n1, n2]; },
-    function(n1, n2) { if (n2[0] < 2 || n1[0] < 2) return false; return [n1[0]*n2[0], '*', n2, n1]; },
-    function(n1, n2) { if (n2[0] < 2 || n1[0]%n2[0] != 0) return false; return [n1[0]/n2[0], '/', n1, n2]; },
-    function(n2, n1) { if (n2[0] < 2 || n1[0]%n2[0] != 0) return false; return [n1[0]/n2[0], '/', n1, n2]; },
+    function(n1, n2) {
+      return [n1[0]+n2[0], '+', n2, n1];
+    },
+    function(n1, n2) {
+      if (n2[0] >= n1[0]) return false;
+      return [n1[0]-n2[0], '-', n1, n2];
+    },
+    function(n2, n1) {
+      if (n2[0] >= n1[0]) return false;
+      return [n1[0]-n2[0], '-', n1, n2];
+    },
+    function(n1, n2) {
+      if (n2[0] < 2 || n1[0] < 2) return false;
+      return [n1[0]*n2[0], '*', n2, n1];
+    },
+    function(n1, n2) {
+      if (n2[0] < 2 || n1[0]%n2[0] != 0) return false;
+      return [n1[0]/n2[0], '/', n1, n2];
+    },
+    function(n2, n1) {
+      if (n2[0] < 2 || n1[0]%n2[0] != 0) return false;
+      return [n1[0]/n2[0], '/', n1, n2];
+    }
 ];
 
 function _recurse_solve_numbers(numbers, searchedi, was_generated, target, levels) {
