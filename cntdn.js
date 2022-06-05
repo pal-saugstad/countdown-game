pal-saugstad/countdown-game

commit 6f8353ffaaa6ab60f748af14ccaca2a8e53fa283

Make sure last version of js-files are used

cntdn.js
@@ -29,7 +29,7 @@ function _calc(vals, show_partial) {
   var big_return = ''
   while (more) {
     more = false;
-    for (idx = 0; idx < vals.length-3 && !more; idx++) {
+    for (let idx = 0; idx < vals.length-3 && !more; idx++) {
       if (!isNaN(vals[idx]) && !isNaN(vals[idx+2])) {
         if (vals[idx+1] == '*') {
           vals[idx] *= vals[idx+2]; more = true;

index.html
@@ -206,10 +206,10 @@
 
     <script src="jquery.min.js"></script>
     <script src="dictionary.js"></script>
-    <script src="dictionary-stats.js"></script>
+    <script src="dictionary-stats.js?v=1.1"></script>
     <script src="letters.js?v=1.4"></script>
     <script src="cntdn.js?v=1.4"></script>
-    <script src="conundrum2.js"></script>
+    <script src="conundrum2.js?v=1.1"></script>
     <script src="practice.js?v=1.4"></script>
 
 

commit 10478bf04846d41f88af297c5adda719a93ad290

Fix regression introduced in 6f8353f

cntdn.js
@@ -27,9 +27,10 @@ function _calc(vals, show_partial) {
   //console.log('Input to _calc ---------');
   var more = true;
   var big_return = ''
+  let idx;
   while (more) {
     more = false;
-    for (let idx = 0; idx < vals.length-3 && !more; idx++) {
+    for (idx = 0; idx < vals.length-3 && !more; idx++) {
       if (!isNaN(vals[idx]) && !isNaN(vals[idx+2])) {
         if (vals[idx+1] == '*') {
           vals[idx] *= vals[idx+2]; more = true;

index.html
@@ -208,7 +208,7 @@
     <script src="dictionary.js"></script>
     <script src="dictionary-stats.js?v=1.1"></script>
     <script src="letters.js?v=1.4"></script>
-    <script src="cntdn.js?v=1.4"></script>
+    <script src="cntdn.js?v=1.5"></script>
     <script src="conundrum2.js?v=1.1"></script>
     <script src="practice.js?v=1.4"></script>
 